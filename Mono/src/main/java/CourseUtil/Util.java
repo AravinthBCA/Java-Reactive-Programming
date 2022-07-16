da559ea4398063d22e49589838f2b925bb11816b
Lenovo <Lenovo@DESKTOP-TAHOKJB>
2022-07-17 00:22:38 +0530

    Mono Project Commit

---
+++ Mono/src/main/java/CourseUtil/Util.java
@@ -0,0 +1,35 @@
+package CourseUtil;
+
+import java.util.function.Consumer;
+import com.github.javafaker.Faker;
+
+public class Util {
+	
+	private static final Faker FAKER = Faker.instance();
+	
+	public static Consumer<Object> onNext(){
+		return o -> System.out.println("Received : "+o);
+	}
+	
+	public static Consumer<Throwable> onError(){
+		return err -> System.out.println("Error : "+err.getMessage());
+	}
+	
+	public static Runnable onComplete(){
+		return () -> System.out.println("Completed...");
+	}
+	
+	
+	public static Faker faker() {
+		return FAKER;
+	}
+	
+	public static void sleepSecond(int seconds) {
+		try {
+			Thread.sleep(seconds * 1000);
+		}
+		catch(InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+}
